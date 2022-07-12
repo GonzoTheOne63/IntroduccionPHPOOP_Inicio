62b10cbb27badefccbd999fe8f01ddb067e1292b
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-12 15:21:05 -0500

    ConfigurandoGitignore
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 02.php
+++ 02.php
@@ -1,4 +1,31 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
+include 'includes/header.php';
 
-include 'includes/footer.php';  
+// ENCAPSULACIÓN
+class Producto // Estos son "atributos o propiedades"
+{
+    public function __construct(public string $nombre, public int $precio, public bool $disponible)
+    {
+    }
+    public function mostrarProducto()
+    {
+        echo "El Product es: " . $this->nombre . "y su precio es de: $" . $this->precio;
+    }
+}
+// Aquí el código del OBJETO
+$producto = new Producto('Tablet 24 pulgadas', 200, true);
+$producto->mostrarProducto();
+    echo "<pre>";
+        var_dump($producto);
+    echo "</pre>";
+
+$producto2 = new Producto('Monitor Curvo 55 pulgadas', 400, true);
+$producto2->mostrarProducto();
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
+
+include 'includes/footer.php';
