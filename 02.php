bb2dad7ef9f50c6eea4d7dcd2aabd567d7daffd3
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-12 21:45:02 -0500

    PublicProtectedPrivate
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 02.php
+++ 02.php
@@ -5,24 +5,34 @@ include 'includes/header.php';
 
 // ENCAPSULACIÓN
 class Producto // Estos son "atributos o propiedades"
-{
-    public function __construct(public string $nombre, public int $precio, public bool $disponible)
+{   ## Propiedad   // public = modificador de acceso se puede modificar tanto en "clase y objeto".
+    ## protected = Se puede acceder y modificar únicamente en la "clase".
+    ## private = solo miembros de la misma clase pueden acceder a él.
+    public function __construct(private string $nombre, public int $precio, public bool $disponible)
     {
     }
-    public function mostrarProducto()
+    public function mostrarProducto(): void ## Método
     {
-        echo "El Product es: " . $this->nombre . "y su precio es de: $" . $this->precio;
+        echo "El Producto es: " . $this->nombre . "y su precio es de: $" . $this->precio;
+    } ## Mientras más cerrado es mejor (principio de la programación)
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+    public function setNombre(string $nombre)
+    {
+        $this->nombre = $nombre;
     }
 }
-// Aquí el código del OBJETO
+// Aquí el código del OBJETO 
 $producto = new Producto('Tablet 24 pulgadas', 200, true);
-$producto->mostrarProducto();
-    echo "<pre>";
-        var_dump($producto);
-    echo "</pre>";
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
 
 $producto2 = new Producto('Monitor Curvo 55 pulgadas', 400, true);
-$producto2->mostrarProducto();
+
 
 echo "<pre>";
 var_dump($producto2);
