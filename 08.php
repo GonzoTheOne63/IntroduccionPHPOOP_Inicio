de89c61702d74de0156d2b329f6ffe9ae6d98c0b
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-14 21:39:20 -0500

    NAMESPACE
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 08.php
+++ 08.php
@@ -4,14 +4,21 @@
 // require 'clases/Clientes.php';
 // require 'clases/Detalles.php';
 
+use App\Clientes; 
+use App\Detalles; 
+
+// Incluir las otras clases
 function mi_autoload($clase) {
-    // echo $clase;
-    require __DIR__ . '/clases/' . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
-spl_autoload_register('mi_autoload');
+ spl_autoload_register('mi_autoload');
 
 $detalles = new Detalles();
 echo "<br>";
 echo "<hr>";
 $clientes = new Clientes();
+echo "<br>";
+echo "<hr>";
+
 include 'includes/footer.php';
