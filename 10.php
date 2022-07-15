1d4c4a207458a2a6745d05f7698e6a7460d2e595
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-15 16:13:45 -0500

    CerrandoPDO
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 10.php
+++ 10.php
@@ -1,4 +1,31 @@
 <?php include 'includes/header.php';
 
+## Conectar a la base de datos con PDO = extensión Objetos de Datos de PHP
+$db = new PDO('mysql:host=localhost; dbname=bienes_raices', 'root', '');
+
+## Creamos el query
+$query = "SELECT titulo, precio, imagen from propiedades";
+
+## Lo preparamos
+$stmt = $db->prepare($query);
+
+## Lo ejecutamos
+$stmt->execute();
+
+## Obetener los resultados
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+## Iterar
+foreach($resultado as $propiedad):
+    echo "<hr>";
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+    echo "</br>";
+    echo $propiedad['precio'];
+    echo "</br>";
+endforeach;
+
 
 include 'includes/footer.php';
