01d8f8ba867780b329b36ebf4fa20b9f351a00df
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-14 19:11:46 -0500

    HERENCIA
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 04.php
+++ 04.php
@@ -1,4 +1,51 @@
 <?php include 'includes/header.php';
 
+## HERENCIA
+class Transporte {
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+    public function getInfo() : string {
+        return "Cantidad de ruedas: " . $this->ruedas .  " Capacidad: " . $this->capacidad . " pasajeros";
+    }
+    public function getRuedas() : string {
+        return $this->ruedas;
+    }
+}
+
+class Bicicleta extends Transporte {
+    public function getInfo(): string
+    {
+        return "    NO UTILIZA COMBUSTIBLES FÓSILES. Cantidad de ruedas: " . $this->ruedas .  " Capacidad: " . $this->capacidad . " pasajeros ";
+    }
+}
+class Vagoneta extends Transporte {
+
+}
+class Automovil extends Transporte {
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
+{    
+}
+    public function getTransmision() : string {
+        return $this->transmision;
+    }
+}
+
+
+$bicicleta = new Bicicleta( 02, 1);
+echo $bicicleta->getInfo();
+echo $bicicleta->getRuedas();
+
+
+echo "<hr>"; ## Añade una líea de separación
+$vagoneta = new Vagoneta(4, 12);
+echo $vagoneta->getInfo();
+
+echo "<hr>";
+$auto = new Automovil(4, 5, ' Transmision Manual');
+echo $auto->getInfo();
+echo $auto->getTransmision();
+
 
 include 'includes/footer.php';
