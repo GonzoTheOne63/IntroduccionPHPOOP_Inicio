26a3ed06f4c6e31946ca5818aca94d9ae581180c
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-14 22:18:20 -0500

    IncompletoCompoer
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 08.php
+++ 08.php
@@ -1,18 +1,21 @@
 <?php include 'includes/header.php';
-// Incluir las otras clases con AUTOLOAD
+
+require 'vendor/autoload.php';
 
 // require 'clases/Clientes.php';
 // require 'clases/Detalles.php';
 
-use App\Clientes; 
-use App\Detalles; 
+use App\Clientes;
+use App\Detalles;
+use Firebase\JWT\Key;
 
 // Incluir las otras clases
-function mi_autoload($clase) {
-    $partes = explode('\\', $clase);
-    require __DIR__ . '/clases/' . $partes[1] . '.php';
-}
- spl_autoload_register('mi_autoload');
+// function mi_autoload($clase) {
+//     $partes = explode('\\', $clase);
+//     require __DIR__ . '/clases/' . $partes[1] . '.php';
+// }
+//  spl_autoload_register('mi_autoload');
+## Todo esto está en la carpeta "VENDOR"
 
 $detalles = new Detalles();
 echo "<br>";
@@ -20,5 +23,41 @@ echo "<hr>";
 $clientes = new Clientes();
 echo "<br>";
 echo "<hr>";
+
+$key = 'example_key';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000
+];
+
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+$jwt = JWT::encode($payload, $key, 'HS256');
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+print_r($decoded);
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+$decoded_array = (array) $decoded;
+
+/**
+ * You can add a leeway to account for when there is a clock skew times between
+ * the signing and verifying servers. It is recommended that this leeway should
+ * not be bigger than a few minutes.
+ *
+ * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+ */
+JWT::$leeway = 60; // $leeway in seconds
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
 include 'includes/footer.php';
