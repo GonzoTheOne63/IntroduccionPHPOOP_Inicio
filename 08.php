fb58de7f487af67021454433c8e3c1cecdf7487c
Gonzalo Barrón <gbarron.ms@gmail.com>
2022-07-14 20:40:33 -0500

    AUTOLOAD
    Signed-off-by: Gonzalo Barrón <gbarron.ms@gmail.com>

--- 08.php
+++ 08.php
@@ -1,4 +1,17 @@
 <?php include 'includes/header.php';
+// Incluir las otras clases con AUTOLOAD
 
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
 
+function mi_autoload($clase) {
+    // echo $clase;
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+echo "<br>";
+echo "<hr>";
+$clientes = new Clientes();
 include 'includes/footer.php';
